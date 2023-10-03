c11c726172b2c88de0e9068b55877f0e15840e12
HumamMajdalawi <humam.almajdalawi@gmail.com>
2023-10-03 18:16:22 +0200

    PR-05/readme-added

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to your Node.js application directory
+cd ..
+
+# Run npm install to install dependencies
+npm install
+
+# Run npm run start to start your Node.js app
+npm run start
